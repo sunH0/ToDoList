0860133f2f6a8993acd18b877c0e9b032c79a605
sunH0 <sunho6824@daum.net>
2022-06-09 22:28:08 +0900

    feat: 에러 처리 로직 구현

---
+++ src/main/java/com/mission/global/ErrorResponse.java
@@ -0,0 +1,87 @@
+package com.mission.global;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mission.global.error.ErrorCode;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+	private String message;
+	private int status;
+	private List<FieldError> errors;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	private LocalDateTime serverDatetime;
+
+	private ErrorResponse(ErrorCode errorCode) {
+		this.message = errorCode.getMessage();
+		this.status = errorCode.getStatus();
+		this.errors = new ArrayList<>();
+	}
+
+	private ErrorResponse(ErrorCode errorCode, List<FieldError> errors) {
+		this.message = errorCode.getMessage();
+		this.status = errorCode.getStatus();
+		this.errors = errors;
+		this.serverDatetime = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode) {
+		return new ErrorResponse(errorCode);
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode, BindingResult bindingResult) {
+		return new ErrorResponse(errorCode, FieldError.of(bindingResult));
+	}
+
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	public static class FieldError {
+
+		private String field;
+		private String value;
+		private String reason;
+
+		private FieldError(final String field, final String value, final String reason) {
+			this.field = field;
+			this.value = value;
+			this.reason = reason;
+		}
+
+		public static List<FieldError> of(
+			final String field,
+			final String value,
+			final String reason
+		) {
+			List<FieldError> fieldErrors = new ArrayList<>();
+			fieldErrors.add(new FieldError(field, value, reason));
+			return fieldErrors;
+		}
+
+		private static List<FieldError> of(final BindingResult bindingResult) {
+			final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+			return fieldErrors
+				.stream()
+				.map(fieldError -> new FieldError(
+					fieldError.getField(),
+					fieldError.getRejectedValue() == null ? "" : fieldError
+						.getRejectedValue()
+						.toString(),
+					fieldError.getDefaultMessage()
+				))
+				.collect(Collectors.toList());
+		}
+	}
+
+}
---
+++ src/main/java/com/mission/global/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.mission.global;
+
+
+import com.mission.global.error.BusinessException;
+import com.mission.global.error.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(BusinessException.class)
+	protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+		log.warn("{} 에러 : {}", e.getErrorCode(), e.getMessage(), e);
+		final ErrorCode errorCode = e.getErrorCode();
+		final ErrorResponse response = ErrorResponse.of(errorCode);
+
+		return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+	}
+
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.warn("handleException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+		MethodArgumentNotValidException e) {
+		log.warn("handleMethodArgumentNotValidException", e);
+		final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE,
+														e.getBindingResult());
+		return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+	}
+
+}
