115bbe6442e6b9d99df3ee88a51da6b21dadb1b0
sunho <sunh072010@gmail.com>
2022-06-10 22:28:27 +0900

    refactor: Cors 설정 수정

--- src/main/java/com/mission/todolist/global/config/WebMvcConfig.java
+++ src/main/java/com/mission/todolist/global/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.mission.todolist.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,7 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry
 			.addMapping("/**")
 			.allowedOrigins("http://localhost:8080")
-			.allowedMethods("*")
-			.allowedHeaders("*");
+			.allowedMethods("GET","POST","PUT","PATCH","DELETE");
 	}
 }
