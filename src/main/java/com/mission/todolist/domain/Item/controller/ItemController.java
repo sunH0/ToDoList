24c093eb38f7cb7d6930950c5423346bd6ceb44e
sunH0 <sunho6824@daum.net>
2022-06-10 14:35:14 +0900

    feat: item controller 구현 & WebMvcConfig 설정

---
+++ src/main/java/com/mission/todolist/domain/Item/controller/ItemController.java
@@ -0,0 +1,61 @@
+package com.mission.todolist.domain.Item.controller;
+
+import com.mission.todolist.domain.Item.dto.ItemRequest;
+import com.mission.todolist.domain.Item.dto.ItemResponse;
+import com.mission.todolist.domain.Item.service.ItemService;
+import com.mission.todolist.domain.common.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
+public class ItemController {
+
+	private final ItemService itemService;
+
+	@PostMapping
+	public ApiResponse<ItemResponse.ItemInfoResponse> addItem(@RequestBody ItemRequest.CreateRequest request){
+		ItemResponse.ItemInfoResponse response = itemService.createItem(request);
+
+		return ApiResponse.ok(response);
+	}
+
+	@GetMapping
+	public ApiResponse<List<ItemResponse.ItemInfoResponse>> getAllTodoByStatus(@RequestParam(name = "status") String status) {
+		List<ItemResponse.ItemInfoResponse> responses = itemService.getAllTodos(status);
+
+		return ApiResponse.ok(responses);
+	}
+
+	@PatchMapping("/{id}/{status}")
+	public ApiResponse<ItemResponse.ItemInfoResponse> changeStatus(@PathVariable(name = "id") long id, @PathVariable(name = "status") String status) {
+		ItemResponse.ItemInfoResponse response = itemService.changeStatus(id, status);
+
+		return ApiResponse.ok(response);
+	}
+
+	@PutMapping("/{id}")
+	public ApiResponse<ItemResponse.ItemInfoResponse> updateTodo(@PathVariable(name = "id") long id, @RequestBody ItemRequest.UpdateRequest content) {
+		ItemResponse.ItemInfoResponse response = itemService.amendComment(id, content);
+
+		return ApiResponse.ok(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ApiResponse<String> deleteTodo(@PathVariable(name = "id") long id) {
+		itemService.removeItem(id);
+		return ApiResponse.ok();
+	}
+
+}
