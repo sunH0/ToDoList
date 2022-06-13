8185e635860fb143ad9628fd9927639d602b3266
sunho <sunh072010@gmail.com>
2022-06-13 16:32:50 +0900

    refactor: enum 입렵값에 대해 소문자 허용

--- src/main/java/com/mission/todolist/domain/Item/entity/ItemStatus.java
+++ src/main/java/com/mission/todolist/domain/Item/entity/ItemStatus.java
@@ -1,17 +1,20 @@
 package com.mission.todolist.domain.Item.entity;
 
+import com.mission.todolist.global.error.BusinessException;
+import com.mission.todolist.global.error.ErrorCode;
+import java.util.Arrays;
+
 public enum ItemStatus {
 	COMPLETE,
 	ACTIVE;
 
-/*	public static ItemStatus findItemStatus(String code){
-		return Arrays
-			.stream(values())
-			.filter(grade -> grade
-				.equals(code))
+	public static ItemStatus of(String status) {
+		return Arrays.stream(values())
+			.filter(code -> code.name().equalsIgnoreCase(status))
 			.findFirst()
-			.orElseThrow(() -> new NotFoundException(ErrorCode.ITEMSTATE_NOT_FOUND));
-	}*/
+			.orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
+
+	}
 
 }
 
