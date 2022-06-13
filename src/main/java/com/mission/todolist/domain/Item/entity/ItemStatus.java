d5613087b3a813c18340dbd6b2043ecbd9d4b75c
sunho <sunh072010@gmail.com>
2022-06-13 22:15:44 +0900

    feat: (사용하지 않은 코드) Map을 사용한 enum 변환 로직 구현

--- src/main/java/com/mission/todolist/domain/Item/entity/ItemStatus.java
+++ src/main/java/com/mission/todolist/domain/Item/entity/ItemStatus.java
@@ -2,7 +2,15 @@ package com.mission.todolist.domain.Item.entity;
 
 import com.mission.todolist.global.error.BusinessException;
 import com.mission.todolist.global.error.ErrorCode;
+import java.time.DayOfWeek;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum ItemStatus {
 	COMPLETE,
@@ -14,6 +22,15 @@ public enum ItemStatus {
 			.findFirst()
 			.orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
 
+	}
+
+	private static final Map<String, ItemStatus> STATUS_MAP =
+		Collections.unmodifiableMap(Stream
+										.of(values())
+										.collect(Collectors.toMap(Enum::name, Function.identity())));
+
+	public static ItemStatus find(String status) {
+		return Optional.ofNullable(STATUS_MAP.get(status)).orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
 	}
 
 }
